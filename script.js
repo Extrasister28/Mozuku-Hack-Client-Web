933c7aaf59f60002ab9d8fc0ac1da38b6507b54d
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2023-02-09 06:39:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
+function init() {
+    window.addEventListener('DOMContentLoaded', testLoad);
+    stopAllLoad();
+}
 
-$(function () {
-  // ローダー終了
-  function end_loader() {
-    $('.loader').fadeOut(800);
-  }
-  // テキスト表示
-  function show_txt() {
-    $('.loader .txt-content .txt').fadeIn(400);
-  }
-  // テキスト非表示
-  function hide_txt() {
-    $('.loader .txt-content .txt').fadeOut(400);
-  }
+function testLoad() {
+        startLoad();
+        setTimeout(stopAllLoad, 3000);
+    }
+}
 
-  // タイマー処理
-  $(window).on('load', function () {
-    // 処理①
-    setTimeout(function () {
-      show_txt();
-    }, 1000)
-    // 処理②
-    setTimeout(function () {
-      hide_txt();
-    }, 3500)
-    // 処理③
-    setTimeout(function () {
-      end_loader();
-    }, 4500)
-  })
-})
+function startLoad() {
+    document.getElementById("loadingwindow").style.visibility = 'visible';
+}
+
+function stopAllLoad() {
+    let elements = document.getElementsByClassName('loading');
+    for (let element of elements) {
+        element.style.visibility = 'hidden';
+    }
+}
+
+window.addEventListener('DOMContentLoaded', init);
