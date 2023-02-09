5c4cc364a54ac3522f39cb4ef04664f0ea801d77
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2023-02-09 09:28:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
     window.addEventListener('Load', testLoad);
-    window.setTimeout(stopAllLoad, 5000);
+    window.setTimeout(fade, 5000);
+    window.setTimeout(stopAllLoad, 8000);
 function testLoad() {
         startLoad();
         stopAllLoad();
@@ -7,6 +8,19 @@ function testLoad() {
 
 function startLoad() {
     document.getElementById("loadingwindow").style.visibility = 'visible';
+}
+
+function fade() {
+    document.querySelector('loadingwindow').animate(
+  [
+    { opacity: 1 },
+    { opacity: 0 }
+  ],
+  {
+    duration: 3000,
+    fill: 'forwards'
+  }
+);
 }
 
 function stopAllLoad() {
