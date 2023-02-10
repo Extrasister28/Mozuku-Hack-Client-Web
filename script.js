86a1fafce75ca7c24691c22c125b4e00efba5903
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2023-02-10 09:15:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let time = Math.floor(Math.random() * 5) + 1;
 window.addEventListener('Load', startLoad);
-window.setTimeout(stopAllLoad, time * 1000 + 6000);
+window.setTimeout(stopAllLoad, time * 1000 + 1000);
 window.setTimeout(fade, time * 1000);
 
 function startLoad() {
